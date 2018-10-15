043d4ad8979b2dcd46e857aa399b8ecdd9bb15e2
刘平 <liuping@misrobot.com>
2018-10-15 17:53:43 +0800

    change scripts path

--- sources/Application.hpp
+++ sources/Application.hpp
@@ -32,14 +32,13 @@ public:
 		// lua
 		struct lua_State *L = luaL_newstate();
 		luaL_openlibs(L);
-		luaL_loadfile(L, "./scritps/main.lua");
+		luaL_loadfile(L, "../scritps/main.lua");
 
 		// 获取函数，压入栈中  
 		lua_getglobal(L, "load");
 		// 调用函数，调用完成以后，会将返回值压入栈中，2表示参数个数，1表示返回结果个数。  
 		if (lua_pcall(L, 0, 0, 0))// 调用出错  
 		{
-			const char *pErrorMsg = lua_tostring(L, -1);
 			spdlog::info(lua_tostring(L, -1));
 		}
 	}
